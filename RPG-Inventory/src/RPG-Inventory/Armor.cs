be3c2172cb24014e9367160d98d68f342f7a0c6d
Sofiia Lavrynenko <sofiia.lavrynenko.25@kse.org.ua>
2026-04-08 11:54:51 +0300

    feat: add Armor class

---
+++ RPG-Inventory/src/RPG-Inventory/Armor.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace RPG_Inventory;
+
+public class Armor: Item
+{
+    public Armor(string title, double weight)
+        : base(title, weight) {}
+
+    public override void Use()
+    {
+        //
+    }
+}
